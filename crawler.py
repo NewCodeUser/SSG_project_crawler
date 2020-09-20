e7ff9d282599d1fe8d804cc63653d250f33ff963
NewCodeUser <wnswl316@naver.com>
2020-09-20 16:05:26 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,16 +1,22 @@
 #-*- coding: utf-8 -*-
 
 import requests
-import time
+from datetime import datetime
+from ptyz 
 import schedule
+import time
 
 num = []
 parking_place = []
 parking_max = []
 parking_now = []
-now_time = []
+now_time = ""
 
 def craw_parking():
+    tmp_num = []
+    tmp_parking_place = []
+    tmp_parking_max = []
+    tmp_parking_now = []
     url = "https://www.sisul.or.kr/open_content/parking/guidance/useable.jsp"
     bring = requests.get(url)
     src = bring.text
@@ -22,29 +28,29 @@ def craw_parking():
         first_data_index = data_list.find("<td>") + 4
         end_data_index = data_list.find(" </td>")
         data_word = data_list[first_data_index:end_data_index]
-        num.append(data_word)
+        tmp_num.append(data_word)
         data_list = data_list[end_data_index+5:]
 
         first_data_index = data_list.find("<td>") + 4
         end_data_index = data_list.find(" </td>")
         data_word = data_list[first_data_index:end_data_index]
-        parking_place.append(data_word)
+        tmp_parking_place.append(data_word)
         data_list = data_list[end_data_index+5:]
 
         first_data_index = data_list.find("<td>") + 4
         end_data_index = data_list.find(" </td>")
         data_word = data_list[first_data_index:end_data_index]
-        parking_max.append(data_word)
+        tmp_parking_max.append(data_word)
         data_list = data_list[end_data_index+5:]
 
         first_data_index = data_list.find("<td>") + 4
         end_data_index = data_list.find(" </td>")
         data_word = data_list[first_data_index:end_data_index]
-        parking_now.append(data_word)
+        tmp_parking_now.append(data_word)
         data_list = data_list[end_data_index+5:]
 
-        current_time = time.strftime("%Y-%m-%d %a %H", time.localtime(time.time()))
-        now_time.append(current_time)
+    current_time = datetime.now(timezone("Asia/Seoul")).strftime("%Y-%m-%d %H")
+    return tmp_num, tmp_parking_place, tmp_parking_max, tmp_parking_now, current_time
 
 def craw_weather():
     url = "https://n.weather.naver.com/"
@@ -59,14 +65,18 @@ def craw_weather():
 
 def save():
     f = open("parking_data.csv", mode="at")
-    craw_parking()
+    num, parking_place, parking_max, parking_now, now_time = craw_parking()
     temperture =  craw_weather()
     for i in range(31):
         f.write( "\n" + num[i] + "," + parking_place[i] + "," + parking_max[i] + "," + parking_now[i] + "," + now_time[i] + "," + temperture)
     f.close
-    print("[+] save done! " + now_time[0])
+    print("[+] save done! " + now_time)
+    
+    p = open("log.txt", mode="at")
+    p.write("save done! " + now_time + "\n")
+    p.close()
 
-schedule.every(60).minutes.do(save)
+schedule.every(3600).seconds.do(save)
 
 while True:
     schedule.run_pending()
